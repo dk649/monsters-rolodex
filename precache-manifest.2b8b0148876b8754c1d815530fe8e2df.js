ef1faa834ceded0b8b01290f2d294958fc2c1638
dk649 <58308212+dk649@users.noreply.github.com>
2020-04-15 16:41:58 +0100

    Updates

---
+++ precache-manifest.2b8b0148876b8754c1d815530fe8e2df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c7fc1410f846dbf32e578ec4531052",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b45ec5e3b96ad5f1d6cc",
+    "url": "/monsters-rolodex/static/css/main.0f12e6e7.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45ec5e3b96ad5f1d6cc",
+    "url": "/monsters-rolodex/static/js/main.013f72d4.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
